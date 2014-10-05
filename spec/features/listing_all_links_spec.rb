f29a7dc904d2e570547f8855aa723c5c93cc1fd4
nikesh <nikeshashar@gmail.com>
2014-10-05 01:43:18 +0100

    Updated tests and code with new syntax

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "User browses the list of links" do
 
   before(:each) {
-    Link.create(:url => "http://www.makersacademy.com", :title => "Makers Academy")
+    Link.create(url: "http://www.makersacademy.com", title: "Makers Academy")
   }
 
   scenario "when opening the home page" do
